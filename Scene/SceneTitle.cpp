49da4e77ffd748f0d43ca8bfcc8fa34e16ba1d09
大野 栞穂 <fko2247078@stu.o-hara.ac.jp>
2022-11-29 18:33:05 +0900

    コメント消去

--- Scene/SceneTitle.cpp
+++ Scene/SceneTitle.cpp
@@ -10,7 +10,6 @@ namespace
 	int kColorbackground = 0;
 	int kColorletter = 255;
 	constexpr int kWaitFrameMin = 120;		//待ち時間
-	//constexpr int kWaitFrameMax = 180;		//待ち時間
 }
 
 SceneTitle::SceneTitle() :
@@ -59,20 +58,12 @@ void SceneTitle::update()
 		kColorletter = 255;
 		m_waitFrame = kWaitFrameMin;
 	}
-
-	//DrawString((Game::kScreenWindth - GetDrawStringWidth(kExplanationText, -1)) / 2, 580, kExplanationText, GetColor(kColor, kColor, kColor));			//タイトル画面の表示
-
 }
 
 void SceneTitle::draw()
 {
 	SetDrawBright(kColorbackground, kColorbackground, kColorbackground);
 	DrawGraph(0, 0, m_handle, true);
-	DrawFormatString(0, 500, GetColor(255, 0, 0), "%d", m_backgroundcolor);
-	DrawFormatString(0, 200, GetColor(255, 0, 0), "w:%d", m_waitFrame);
-	DrawFormatString(0, 255, GetColor(255, 0, 0), "%d", kColorletter);
-	DrawFormatString(0, 300, GetColor(255, 0, 0), "w:%d", kColorbackground);
 	DrawString((Game::kScreenWindth - GetDrawStringWidth(kTitleText, -1)) / 2, 480, kTitleText, GetColor(255, 255, 255));			//タイトル画面の表示
 	DrawString((Game::kScreenWindth - GetDrawStringWidth(kExplanationText, -1)) / 2, 580, kExplanationText, GetColor(kColorletter, kColorletter, kColorletter));			//タイトル画面の表示
-	//DrawString((Game::kScreenWindth - GetDrawStringWidth(kExplanationText, -1)) / 2, 580, kExplanationText, GetColor(kColor, kColor, kColor));			//タイトル画面の表示
 }
