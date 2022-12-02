b92df0d44f792623238513fb7c935f250fd71a3e
大野 栞穂 <fko2247078@stu.o-hara.ac.jp>
2022-12-02 13:55:24 +0900

    コメントの追加

--- Object/Player.cpp
+++ Object/Player.cpp
@@ -7,9 +7,6 @@ namespace
 {
 	constexpr float kJumpAcc = -21.0f;			//ジャンプ力
 	constexpr float kGravity = 0.6f;			//重力
-
-	//constexpr int kWaitFrameMin = 60;		//待ち時間
-	//constexpr int kWaitFrameMax = 180;		//待ち時間
 }
 
 Player::Player() :
@@ -18,8 +15,7 @@ Player::Player() :
 	m_size(),
 	m_isDead(),
 	m_fieldY(),
-	m_handle(-1)/*,
-	m_waitFrame(0)*/
+	m_handle(-1)
 {
 
 }
@@ -31,61 +27,47 @@ Player::~Player()
 
 void Player::init()
 {
-	//m_waitFrame = GetRand(kWaitFrameMax) + kWaitFrameMin;
-	m_pos.x = 40;
-	//m_pos.y = Game::kScreenHeight - m_fieldY;
-	m_pos.y = 590;
-	m_vec.x = 0.0f;
-	m_vec.y = 0.0f;
+	m_pos.x = 40;			//プレイヤーの初期位置
+	m_pos.y = 590;			//プレイヤーの初期位置
+	m_vec.x = 0.0f;			//プレイヤーのJump速度
+	m_vec.y = 0.0f;			//プレイヤーのJump速度
 
-	//m_handle = LoadGraph("date/usa_Normal.png");
-	//m_handle = LoadGraph("date/usa_Over.png");
-
-	GetGraphSizeF(m_handle, &m_size.x, &m_size.y);
-
+	GetGraphSizeF(m_handle, &m_size.x, &m_size.y);		//プレイヤーのグラフサイズの取得
 }
 
 void Player::updata()
 {
-	/*if (m_waitFrame > 0)
-	{
-		m_waitFrame--;
-		return;
-	}*/
-	if (m_isDead) return;
+	if (m_isDead) return;		//プレイヤーが死亡していたら以下の処理を行わない
 
-	m_pos += m_vec;
+	m_pos += m_vec;				//プレイヤーのJump速度
+
 	//地面との当たり
-
 	bool isField = false;
 
-	if (m_pos.y > m_fieldY - m_size.y)
+	if (m_pos.y > m_fieldY - m_size.y)			//床に当たっていたらJumpする
 	{
 		m_pos.y = m_fieldY - m_size.y;
-		isField = true;
+		isField = true;							//飛んでいる間はtrueを返す
 	}
 
 	// キー入力処理
 
-	if (Pad::isPress(PAD_INPUT_1))
-		//if (Pad::isTrigger(PAD_INPUT_1))
+	if (Pad::isPress(PAD_INPUT_1))				//ボタンを押されたらJumpをする
 	{
-		if (isField)
+		if (isField)						//床に当たっていたら処理をする
 		{
 			m_vec.y = kJumpAcc;				//ジャンプ開始
 		}
 	}
-	m_vec.y += kGravity;
-
-
+	m_vec.y += kGravity;					//重力を足す
 }
 
 void Player::draw()
 {
-	if (m_isDead)
+	if (m_isDead)							//死んでいたら処理を行わない
 	{
-		DrawGraph(m_pos.x, m_pos.y, m_handle, true);
+		DrawGraph(m_pos.x, m_pos.y, m_handle, true);		//死亡していた場合の画像表示
 		return;
 	}
-	DrawGraph(m_pos.x, m_pos.y, m_handle, true);
+	DrawGraph(m_pos.x, m_pos.y, m_handle, true);			//画像表示
 }
--- Object/Player.h
+++ Object/Player.h
@@ -7,28 +7,28 @@ public:
 	Player();
 	~Player();
 
-	virtual void init();
+	virtual void init();		//プレイヤーの初期化
 	virtual void end() {}
-	void setGraph(int handle) { m_handle = handle; }
-	virtual void updata();
-	virtual void draw();
+	void setGraph(int handle) { m_handle = handle; }		//プレイヤーの画像セット
+	virtual void updata();			//プレイヤーの更新処理
+	virtual void draw();			//プレイヤーの描画
 
-	void Dead(bool isDead) { m_isDead = isDead; }
+	void Dead(bool isDead) { m_isDead = isDead; }			//プレイヤーの死亡判定
 
-	virtual void setup(float fieldY) { m_fieldY = fieldY; }
+	virtual void setup(float fieldY) { m_fieldY = fieldY; }	//床の判定
 
 	Vec2 getPos() const { return m_pos; }		//情報の取得
 	Vec2 getBottomRight() const { return m_pos + m_size; }		//右上座標の取得
 
-	bool isDead()const { return m_isDead; }
+	bool isDead()const { return m_isDead; }					//プレイヤーの死亡判定
 
 private:
-	Vec2 m_pos;
-	Vec2 m_vec;
-	Vec2 m_size;
-	int m_handle;
+	Vec2 m_pos;			//プレイヤーポジション
+	Vec2 m_vec;			//プレイヤーのJump処理に使用
+	Vec2 m_size;		//グラフサイズ
+	int m_handle;		//プレイヤーハンドル
 
-	bool m_isDead;
+	bool m_isDead;		//プレイヤー死亡フラグ
 
-	float m_fieldY;
+	float m_fieldY;		//床の線のうけわたし
 };
